193caf167bf6891c8fcc8c92483b4e865d713cfb
vanajaSoo73 <113160361+vanajaSoo73@users.noreply.github.com>
2022-11-29 18:05:08 +0530

    Login validation

--- frisky/source/components/login.js
+++ frisky/source/components/login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Icon from 'react-native-vector-icons/AntDesign';
 import {
     StyleSheet,
@@ -12,6 +12,60 @@ import {
 
 
 export default function Logform() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [checkValidEmail, setCheckValidEmail] = useState(false)
+
+    const handleCheckEmail = (text) => {
+        let re = /\S+@\S+\.S+/;
+        let regex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,20}$/;
+
+
+        setEmail(text);
+        if (re.test(text) || regex.test(text)) {
+            setCheckValidEmail(false);
+        } else {
+            setCheckValidEmail(true);
+        }
+
+    };
+     
+
+    const checkPasswordValidity = value => {
+        const isNonWhiteSpace = /^\S*$/;
+        if (!isNonWhiteSpace.test(value)) {
+            return 'password must not contain whitespace.';
+        }
+        const isContainsLowercase = /^(?=.*[a-z]).*$/;
+        if (!isContainsLowercase.test(value)) {
+            return 'password must have at least one lowercase character.';
+        }
+        const isContainsUppercase = /^(?=.*[A-Z]).*$/;
+        if (!isContainsUppercase.test(value)) {
+            return 'password must have at least one uppercase character.';
+        }
+        const isContainsNumber = /^(?=.*[0-9]).*$/;
+        if (!isContainsNumber.test(value)) {
+            return 'password must contain atleast one degit .';
+        }
+        const isValidLength = /^.{8,16}$/;
+        if (!isValidLength.test(value)) {
+            return 'password must be 8-16 character long .';
+        }
+        return null;
+    };
+
+    const handleLogin = () => {
+        const checkPassword = checkPasswordValidity(password);
+        if (!checkPassword) {
+            alert('Success Login');
+        } else {
+            alert(checkPassword);
+        }
+    };
+
+
+
     const renderHeader = () => {
         return (
 
@@ -19,11 +73,12 @@ export default function Logform() {
                 <Icon name='arrowleft' size={35} color={"white"} />
                 <Text style={styles.header}>Login</Text>
             </View>
-
-
-
         )
-    }
+    };
+
+
+
+
     return (
         <View>
 
@@ -38,14 +93,21 @@ export default function Logform() {
                 <Image style={styles.img} source={require('./letter-f-logo-with-musical-note-vector-37337029-removebg-preview.png')} />
             </View>
             <View style={styles.center}>
-                <TextInput style={styles.textinput} placeholder="Username or Email" underlineColorAndroid={'transparent'} />
-                <TextInput style={styles.textinput} placeholder="Password" underlineColorAndroid={'transparent'} />
+                <TextInput style={styles.textinput} placeholder="Username or Email" underlineColorAndroid={'transparent'} value={email} onChangeText={handleCheckEmail} />
+                {checkValidEmail ?
+                    <Text style={styles.textfailed}>Enter valid email</Text> :
+                    <Text style={styles.textfailed}></Text>
+                }
+
+                <TextInput style={styles.textinput} placeholder="Password" underlineColorAndroid={'transparent'} value={password} onChangeText={text => setPassword(text)} secureTextEntry={true} />
 
             </View>
             <View style={styles.btn}>
-                <TouchableOpacity style={styles.button}>
+
+                <TouchableOpacity style={styles.button} onPress={handleLogin}>
                     <Text style={styles.btntext}>Sign In</Text>
                 </TouchableOpacity>
+
             </View >
             <View style={styles.bottomtxt}>
                 <Text style={styles.fp}>Forget Password?</Text>
@@ -81,9 +143,13 @@ const styles = StyleSheet.create({
     center: {
         paddingRight: 50,
         paddingLeft: 50,
+        textAlign: 'center',
+        paddingTop:20,
+        
+
     },
     header: {
-        
+
         fontSize: 25,
         color: 'white',
         paddingRight: 170
@@ -110,17 +176,19 @@ const styles = StyleSheet.create({
     textinput: {
         fontSize: 20,
         height: 50,
-        color: "white",
+        color: "black",
         marginBottom: 5,
         borderBottomColor: 'black',
         borderBottomWidth: 1,
         paddingTop: 20,
         paddingBottom: 5,
-        paddingLeft: 50,
-        paddingRight: 50,
-        textAlign: 'center',
-        marginTop: 40
+        paddingLeft: 1,
+        textAlign: "center",
+
+        marginTop: 30,
+
     },
+
     button: {
         alignItems: 'center',
         padding: 20,
@@ -129,7 +197,8 @@ const styles = StyleSheet.create({
     },
     btntext: {
         color: '#fff',
-        fontWeight: 'bold'
+        fontWeight: 'bold',
+        
     },
 
     logo: {
@@ -156,6 +225,11 @@ const styles = StyleSheet.create({
         marginTop: 0.1
 
 
+    },
+    textfailed: {
+        color: 'red',
+        textAlign: 'right',
+    
     }
 
 
