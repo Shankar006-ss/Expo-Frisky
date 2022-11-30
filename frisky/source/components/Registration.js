1092e237463e2747d103f8d54255dd82a27a54ff
varshini76 <116704478+varshini76@users.noreply.github.com>
2022-11-30 10:29:21 +0530

    register validation

--- frisky/source/components/Registration.js
+++ frisky/source/components/Registration.js
@@ -8,7 +8,17 @@ import {
   Image,
   StatusBar,
 } from 'react-native';
+<<<<<<< Updated upstream
 import Icon from 'react-native-vector-icons/AntDesign';
+=======
+
+import Icon from 'react-native-vector-icons/AntDesign';
+
+
+
+
+
+>>>>>>> Stashed changes
 export default function Regs() {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
@@ -17,6 +27,11 @@ export default function Regs() {
   const [Name, setname] = useState("")
   const [checkValidpass, setCheckValidpass] = useState(false)
   const [Confirm, setConfirmpass] = useState(false)
+<<<<<<< Updated upstream
+=======
+
+
+>>>>>>> Stashed changes
   //name field validation
   const handlename = (text) => {
     let rule = /^[a-zA-Z]{8}$/;
@@ -25,12 +40,24 @@ export default function Regs() {
       setCheckValidateName(false);
     } else {
       setCheckValidateName(true);
+<<<<<<< Updated upstream
     }
   };
+=======
+
+    }
+  };
+
+
+>>>>>>> Stashed changes
   //Email field validation
   const handleCheckEmail = (text) => {
     let re = /\S+@\S+\.\S+/;
     let regex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
     setEmail(text)
     if (re.test(text) || regex.test(text)) {
       setCheckValidateEmail(false);
@@ -38,30 +65,58 @@ export default function Regs() {
       setCheckValidateEmail(true);
     }
   };
+<<<<<<< Updated upstream
+=======
+
+
+>>>>>>> Stashed changes
   //password field
   const checkPasswordValidity = value => {
     const isNonWhiteSpace = /^\S*$/;
     if (!isNonWhiteSpace.test(value)) {
       return 'password must not contain whitespace.';
     }
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
     const isContainsLowercase = /^(?=.*[a-z]).*$/;
     if (!isContainsLowercase.test(value)) {
       return 'password must have at least one lowercase character.';
     }
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
     const isContainsUppercase = /^(?=.*[A-Z]).*$/;
     if (!isContainsUppercase.test(value)) {
       return 'password must have at least one uppercase character.';
     }
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
     const isContainsNumber = /^(?=.*[0-9]).*$/;
     if (!isContainsNumber.test(value)) {
       return 'password must contain atleast one degit .';
     }
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
     const isValidLength = /^.{8,16}$/;
     if (!isValidLength.test(value)) {
       return 'password must be 8-16 character long .';
     }
+<<<<<<< Updated upstream
     return null;
   };
+=======
+
+    return null;
+  };
+
+>>>>>>> Stashed changes
   //confirm password
   // const setConfirm = (text) => {
   //   let rule = password;
@@ -73,6 +128,11 @@ export default function Regs() {
   //     setCheckValidpass(true);
   //   }
   // }
+<<<<<<< Updated upstream
+=======
+
+
+>>>>>>> Stashed changes
   //signup button
   const handleregis = () => {
     const checkPassword = checkPasswordValidity(password)
@@ -81,8 +141,19 @@ export default function Regs() {
       alert("Success registration")
     } else {
       alert(checkPassword)
+<<<<<<< Updated upstream
     }
   };
+=======
+
+    }
+
+  };
+
+  
+
+  
+>>>>>>> Stashed changes
   const renderHeader = () => {
     return (
       <View style={styles.container}>
@@ -91,7 +162,10 @@ export default function Regs() {
       </View>
     )
   }
+
+
   return (
+
     <View>
       <View>
         {renderHeader()}
@@ -100,15 +174,27 @@ export default function Regs() {
       <View style={styles.image}>
       <Image style={styles.img} source={require('./logo.png')} />
       </View>
+
       <View style={styles.line}>
         <TextInput style={styles.textinput} placeholder="Name" value={Name}
+<<<<<<< Updated upstream
           onChange={(e => setname(e.target.value))}
           onChangeText={(text) => handlename(text)} underlineColorAndroid={'transparent'} />
+=======
+          onChange={(e => setname(e.target.value))} 
+          onChangeText={(text) => handlename(text)} underlineColorAndroid={'transparent'} />
+
+>>>>>>> Stashed changes
                {checkValidName ? (
                 <Text style={styles.textFailed}>Name is required</Text>
               ) : (
                 <Text style={styles.textFailed}></Text>
               )}
+<<<<<<< Updated upstream
+=======
+
+
+>>>>>>> Stashed changes
         <TextInput style={styles.textinput} placeholder="Email" value={email}
           onChangeText={(text) => handleCheckEmail(text)} underlineColorAndroid={'transparent'} />
                {checkValidEmail ? (
@@ -116,6 +202,7 @@ export default function Regs() {
               ) : (
                 <Text style={styles.textFailed}></Text>
               )}
+<<<<<<< Updated upstream
         <TextInput style={styles.pass} placeholder="Password" value={password} 
           onChangeText={(text) => setPassword(text)} secureTextEntry={true}
           underlineColorAndroid={'transparent'} />
@@ -136,12 +223,50 @@ export default function Regs() {
           <Text style={styles.btntext}>Register</Text>
         </TouchableOpacity>
         ) }
+=======
+
+        <TextInput style={styles.pass} placeholder="Password" value={password} 
+          onChangeText={(text) => setPassword(text)} secureTextEntry={true} 
+          underlineColorAndroid={'transparent'} />
+
+        <TextInput style={styles.text} placeholder="ConfirmPassword" value={Confirm} 
+        onChange={(e => setConfirmpass(e.target.value))} 
+          onChangeText={(text) => setConfirm(text)} secureTextEntry={true} 
+          underlineColorAndroid={'transparent'} />
+                {email ==""|| password == true? (
+                  <Text style={styles.textFailed}>Enter valid password</Text>
+                ) : (
+                  <Text style={styles.textFailed}></Text>
+                )}
+
+                {email ==""|| password == "" || Name =="" || checkValidEmail == true ?(
+
+        <TouchableOpacity
+        disabled
+        style={styles.button} 
+        onPress={handleregis}>
+          <Text style={styles.btntext}>Register</Text>
+        </TouchableOpacity> 
+        ): (
+        <TouchableOpacity style={styles.button} onPress={handleregis}>
+          <Text style={styles.btntext}>Register</Text>
+        </TouchableOpacity>
+        ) }
+
+>>>>>>> Stashed changes
         <Text style={styles.bottom}>Already a user?Login here</Text>
       </View>
     </View>
   );
 }
+<<<<<<< Updated upstream
 const styles = StyleSheet.create({
+=======
+
+
+const styles = StyleSheet.create({
+
+>>>>>>> Stashed changes
   container: {
     width: "100%",
     height: 100,
@@ -158,6 +283,11 @@ const styles = StyleSheet.create({
     fontSize: 25,
     color: 'white',
     paddingRight: 170,
+<<<<<<< Updated upstream
+=======
+
+
+>>>>>>> Stashed changes
   },
   line: {
     paddingLeft: 30,
@@ -194,6 +324,11 @@ const styles = StyleSheet.create({
     paddingBottom: 5,
     paddingLeft: 20,
     textAlign: 'center',
+<<<<<<< Updated upstream
+=======
+
+
+>>>>>>> Stashed changes
   },
   button: {
     alignItems: 'center',
@@ -223,6 +358,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginTop: 30,
     paddingLeft: 130,
+<<<<<<< Updated upstream
+=======
+    
+
+>>>>>>> Stashed changes
   },
   image: {
     height: 100,
@@ -233,7 +373,13 @@ const styles = StyleSheet.create({
   img: {
     height: 100,
     width: 100,
+<<<<<<< Updated upstream
   },
+=======
+
+  },
+
+>>>>>>> Stashed changes
   textFailed: {
     color: 'red',
     paddingLeft: 220,
